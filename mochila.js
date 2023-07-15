7ac760ae8a6a50bcce057f55362db7441718a94e
Luiz Carlos Ferreira <84511085+luizferreira7@users.noreply.github.com>
2023-07-15 11:32:49 -0300

    Criando logica para sortear e desenhar tetromino
    Co-Authored-By: Vitória Chaves <69561315+mendesv1t@users.noreply.github.com>
    Co-Authored-By: Thalita de Freitas Silva <104738980+thalifreitas@users.noreply.github.com>
    Co-Authored-By: Vitória Chaves <69561315+mendesv1t@users.noreply.github.com>
    Co-Authored-By: Thalita de Freitas Silva <104738980+thalifreitas@users.noreply.github.com>

---
+++ mochila.js
@@ -0,0 +1,45 @@
+class Mochila {
+
+    constructor() {
+        this.reiniciar();
+    }
+
+    getIndex() {
+        let tetromino = this.tetrominos.pop();
+
+        if (this.tetrominos.length === 0) {
+            this.reiniciar();
+        }
+
+        return tetromino;
+    }
+
+    getTetrominos() {
+        return [0,1,2,3,4,5,6];
+    }
+
+    reiniciar() {
+        this.tetrominos = this.getTetrominos();
+        
+        this.embaralhar(this.tetrominos);
+    }
+
+    embaralhar(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+    
+}
---
+++ tetromino.js
@@ -0,0 +1,27 @@
+class Tetromino {  
+
+    constructor(ctx, index) {
+        this.ctx = ctx;
+        
+        this.proximaPeca(index);
+    }
+
+    proximaPeca(index) {
+        this.formato = TETROMINOS[index];
+        this.cor = CORES[index];
+        this.x = 3;
+        this.y = 0;
+      }
+
+    desenha() {
+        this.ctx.fillStyle = this.cor;
+        this.formato.forEach((row, y) => {
+          row.forEach((value, x) => {
+
+            if (value > 0) {
+              this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+            }
+          });
+        });
+      }
+}
